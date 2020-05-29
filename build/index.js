0188f46fdae04364efbd5ecd2e76798def82662b
Dominique <dominique.lopez@digitregroup.com>
2020-05-29 11:32:05 +0200

    final refactoring

--- build/index.js
+++ build/index.js
@@ -1,14 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var MatchReader_1 = require("./MatchReader");
-var CsvFileReader_1 = require("./CsvFileReader");
-var ConsoleReport_1 = require("./reportTargets/ConsoleReport");
-var WinsAnalysis_1 = require("./analysers/WinsAnalysis");
 var Summary_1 = require("./Summary");
-var reader = new CsvFileReader_1.CsvFileReader('football.csv');
-var matches = new MatchReader_1.MatchReader(reader);
+var matches = MatchReader_1.MatchReader.fromCsv('football.csv');
+var summaryHtmlReport = Summary_1.Summary.winsAnalysisWithHtmlReport('Chelsea');
 matches.load();
-// console.log(matches.matches);
-//const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
-var summary = new Summary_1.Summary(new WinsAnalysis_1.WinsAnalysis('Man United'), new ConsoleReport_1.ConsoleReport());
-summary.buildAndPrintReport(matches.matches);
+summaryHtmlReport.buildAndPrintReport(matches.matches);
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,8 @@
 import { MatchReader } from './MatchReader';
-import { CsvFileReader } from './CsvFileReader';
-import { ConsoleReport } from './reportTargets/ConsoleReport';
-import { HtmlReport } from './reportTargets/HtmlReport';
-import { WinsAnalysis } from './analysers/WinsAnalysis';
 import { Summary } from './Summary';
 
-const reader = new CsvFileReader('football.csv');
-const matches = new MatchReader(reader);
+const matches = MatchReader.fromCsv('football.csv');
+const summaryHtmlReport = Summary.winsAnalysisWithHtmlReport('Chelsea');
+
 matches.load();
-// console.log(matches.matches);
-
-//const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
-const summary = new Summary(
-  new WinsAnalysis('Man United'),
-  new ConsoleReport()
-);
-
-summary.buildAndPrintReport(matches.matches);
+summaryHtmlReport.buildAndPrintReport(matches.matches);
